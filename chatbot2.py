9253b7d5a87a45d2367406a18810010a8ba1ec77
Sharanya M Kumar <117572506+sharanya1507@users.noreply.github.com>
2022-11-19 12:25:47 +0530

    Add files via upload

---
+++ chatbot2.py
@@ -0,0 +1,15 @@
+print("Hello, i am a chatbot")
+print("What fleshy fruits would you like to buy\n")
+print("Hit 1 for Drupes ")
+print("Hit 2 for Berries ")
+
+userinput = int(input("enter your choice: "))
+
+if userinput == 1:
+    print(" your Drupes will be delivered in 20 minutes")
+if userinput == 2:
+    print(" your Berries will be delivered in 20 minutes")
+else:
+    print(" please provide other request")
+
+print("Thankyou for using our service")
